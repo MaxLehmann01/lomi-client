098cebc6914237ddb9ef09c9c0db25477d04f912
MaxLehmann01 <85064456+MaxLehmann01@users.noreply.github.com>
2026-05-09 11:37:39 +0200

    Added: react as renderer + set fixed compatible package versions

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,5 +1,6 @@
 import { resolve } from 'path';
 import { defineConfig } from 'electron-vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     main: {
@@ -23,5 +24,6 @@ export default defineConfig({
                 '@renderer': resolve('src/renderer/src'),
             },
         },
+        plugins: [react()],
     },
 });
