1ffa17fd567d428e1a1d957c0af5dc925d07b575
MaxLehmann01 <85064456+MaxLehmann01@users.noreply.github.com>
2026-05-09 11:40:30 +0200

    Added: tailwindcss

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,3 +1,7 @@
 export default function App() {
-    return <h1>Hello World from React!</h1>;
+    return (
+        <h1 className={'text-4xl p-2 text-red-900'}>
+            Hello World from React + TailwindCSS!
+        </h1>
+    );
 }
